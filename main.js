5575d3b1b276f44baed22dcee6284f1df2ec4342
Your Name <moisesjuniorcas20@gmail.com>
2023-02-28 08:23:53 -0500

    conectando formulario, css, iconos redes sociales y contacto

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
 function enviarFormulario() {
-    var nombre = document.getElementsByName("nombre")[0].value;
-    var correo = document.getElementsByName("correo")[0].value;
-    var telefono = document.getElementsByName("telefono")[0].value;
-  
-    var link = "moisesjuniorcas20@gmail.com" + 
-               "?subject=" + encodeURIComponent("Nuevo mensaje de formulario") +
-               "&body=" + encodeURIComponent("Nombre: " + nombre + "\nCorreo: " + correo + "\nTeléfono: " + telefono);
-  
-    window.location.href = link;
-  }
+  // Obtenemos los valores de los inputs del formulario
+  const nombre = document.querySelector('input[name="nombre"]').value;
+  const correo = document.querySelector('input[name="correo"]').value;
+  const telefono = document.querySelector('input[name="telefono"]').value;
+
+  // Creamos un objeto con los datos del formulario
+  const formData = {
+    nombre: nombre,
+    correo: correo,
+    telefono: telefono,
+  };
+
+  // Enviamos los datos mediante la API Fetch de JavaScript
+  fetch('https://formspree.io/f/xgebkqyz', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if (response.ok) {
+      // Si la respuesta es satisfactoria, mostramos un mensaje al usuario
+      alert('Gracias por enviar el formulario!');
+    } else {
+      // Si hay un error, mostramos un mensaje de error
+      alert('Hubo un error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
+    }
+  })
+  .catch(error => {
+    // Si hay un error, mostramos un mensaje de error
+    alert('Hubo un error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
+  });
+
+  // Prevenimos que el formulario se envíe por defecto
+  event.preventDefault();
+}
+
   
