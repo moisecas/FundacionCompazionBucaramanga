e3d9845d3cfc73065c23c158f529a3e4c88532ca
unknown <moisesjuniorcas20@gmail.com>
2023-03-20 16:32:21 -0500

    paginas faltantes, enlaces, nav, css, galería videos

---
+++ videos.js
@@ -0,0 +1,10 @@
+$('.galeria-videos').slick({
+    arrows: true,
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+  
